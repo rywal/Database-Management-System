3d51d73741f80af4c759b84735b37d00602df26f
nathan.blattman <nathan.blattman@linux2.cs.tamu.edu>
2016-01-30 12:37:55 -0600

    I added a new file

---
+++ Database.h
@@ -0,0 +1,30 @@
+//This header file will create a class for each 
+#include <stdexcept>
+
+class Domain{		//
+
+} 
+
+class Attribute{	//
+
+}
+
+class Tuple{		//
+}
+
+class Relation {	//
+
+}
+		
+class Database{
+	//dynamic data structure for holding different relations
+	//a function that creates a new relation
+
+}
+
+
+
+
+
+
+
