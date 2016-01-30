661790955ca660cd251a2703169c0a1b7506d6f8
jonygrimes <jonygrimes@tamu.edu>
2016-01-30 14:12:21 -0600

    Added more comments
    I did not add any more code. I just added more comments to understand how the project should be deployed, according to how I understood the project.

--- Database.h
+++ Database.h
@@ -1,22 +1,28 @@
 //This header file will create a class for each 
 #include <stdexcept>
 
-class Domain{		//
-
-} 
-
-class Attribute{	//
+class Domain{		//The value of Atrributes
 
 }
 
-class Tuple{		//
+class Attribute{	//The column (collection of indexes of Tuples)
+//Vector<Domain>       (I'm not sure if this should be a pointer/address)
+
 }
 
-class Relation {	//
+class Tuple{		//The row (fundation of Database)
+//Vector<Domain>
+
+}
+
+class Relation {	//Table that holds Columns and rows
+//Vector<Attribute<Domain>> (I'm not sure what the type would be.)
+//We would have to traverse through the Attribues, first, then we could select which Cell we want, by selecting the Tuple
+//I think?
 
 }
 		
-class Database{
+class Database{         //A collection of Tables
 	//dynamic data structure for holding different relations
 	//a function that creates a new relation
 
