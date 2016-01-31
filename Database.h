192acaac91981af2da96d8496a0b3b54363f71b6
jonygrimes <jonygrimes@tamu.edu>
2016-01-30 21:40:24 -0600

    More shell function in Relation

--- Database.h
+++ Database.h
@@ -25,7 +25,8 @@ public:
 bool doesexist(string att_name){} //Used to see if an Attribute exists
 bool compare(string att_name, string compare_string, int index){} //Used to compare a string to each Domain in an Atrribute
 int getsize(){} //Used to get the number of Tuples in a Relation
-Attribute getatt(int index){} //Used to get Attribute (the data that it connects to) at index
+Atrribute getatt(int index){} //Used to get Attribute (data) at index (will possibly merge with ch_att_name?)
+void ch_att_name(string renamed, int index){} //Used to change Attribute name by index
 void insert(Tuple &tup_name){} //Used to insert a Tuple into a Relation
 void insert(string att_name){} //Used to insert an attribute into the next index of a Relation(possibly union instead??)
 
