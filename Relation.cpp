16ffc45908df4382e6de0c1689f310e5f27aa848
Jonathan Grimes <jonygrimes@tamu.edu>
2016-02-01 21:22:36 -0600

    Updated Relation I correct the format and have updated the 1st function

--- Relation.cpp
+++ Relation.cpp
@@ -8,9 +8,28 @@ Relation::Relation(
              int[]    attribute_max_lengths,   // Max length of attribute string value. NOTE: Should be 0 if type if integer
              string[] primary_keys             // Collection of primary keys
     ){
-	relation_name= name;
-	string[] att_names= attribute_names;
-    	int[] att_max_lengths= attribute_max lengths;
+		relation_name= name;
+		string[] att_names= attribute_names;
+    	int[] att_max_lengths= attribute_max_lengths;//ADDED UNDERSCORE
     	string[] prim_keys= primary_keys;
-	num_attributes= attribute_names.size();
-}
+		num_attributes= attribute_names.size();
+	}
+	
+	bool attribute_exist(string att_name){
+		for (int i=0; i < att_names.size();i++){
+			if (att_names[i] == att_name){
+				return true;
+			}
+		} 
+		return false;
+	}
+	
+    bool compare(vector<int> &tuple_indexes, auto comparison_value, string compare_operator, int index);
+	int get_length();//Number of Columns
+    Attribute get_attribute( int index );
+	int get_attribute_index( string att_name );
+	string get_attribute_name( int index );
+    void rename_attribute( string renamed, int index );
+    void insert( Tuple &tup_name );
+    void insert( Attribute &att_name );
+	
