dfe086955539a551e36428d82c6e1ea06894024d
Jonathan Grimes <jonygrimes@tamu.edu>
2016-02-03 17:48:45 -0600

    Move DBCore to Database.h

--- Database.h
+++ Database.h
@@ -18,13 +18,14 @@ public:
     Database(string name);
     void create_relation(string name, string[] attribute_names, int[] attribute_types, string[] primary_keys);
     std::vector<Tuple> show_relation(string relation_name);
-    
     void insert_tuple(string relation_name);
-    
     bool union_compatible(Relation a, Relation b);
     bool cross_compatible(Relation a, Relation b);
     Relation set_union(string name, Relation a, Relation b);
     Relation set_difference(string name, Relation a, Relation b);
     Relation cross_product(string name, Relation a, Relation b);
+	Relation select(vector<string> att_names, vector<auto> compare_values, vector<string> compare_operators, Relation &in_rel, string and_or_gate);
+	Relation Project(vector<string> att_names, Relation &in_rel);
+	Relation Renaming(String out_rel, vector<string> att_renames , Relation &in_rel);
+};	
 
-};
