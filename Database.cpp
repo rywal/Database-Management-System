2cb6b14804d0ceca63e336839c32044eea924cda
Nathan Blattman <nathan.blattman@linux2.cs.tamu.edu>
2016-02-04 01:38:56 -0600

    update in the program

--- Database.cpp
+++ Database.cpp
@@ -237,17 +237,16 @@ Relation Database::renaming(string out_name, vector<string> att_renames , Relati
 	}
 	return out_rel;
 }
-void Database::update(Relation &in_rel, vector<string> att_names, string compare_operators, vector<auto> comparison_values){
+void Database::update(Relation &in_rel, vector<string> att_names, vector<string> compare_operators, vector<string> comparison_values, vector<string> update_name){
 	vector<int> tuple_indexes;
 	for(int i=0; i<att_names.size();i++){
 		in_rel.compare(tuple_indexes,comparison_values[i],compare_operators[i],in_rel.get_attribute_index(att_names[i]));
-		for(int n=0; n<tuple_indexes;n++){
-			in_rel.tuples[n].cells[in_rel.get_attribute_index(att_names[i])].set_value(compare_values[i]);
+		for(int n=0; n<tuple_indexes.size();n++){
+			in_rel.tuples[n].cells[in_rel.get_attribute_index(att_names[i])].set_value(update_name[i]);
 		}
 		tuple_indexes.clear();
 	}
-}
-/*
+}/*
 void CLOSE(FILE *f){		This needs to close out the database
 	fclose (f);
 }
