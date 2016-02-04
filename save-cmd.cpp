107a20c07ad44e4c16b3b3ae7e52354ae7a16697
Victor Vazquez <victormanuel@tamu.edu>
2016-02-03 22:02:15 -0600

    added save-cmd

---
+++ save-cmd.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+template<class V>
+void vector_out(const vector<V>& vector){
+	cout << "Data: ";
+	copy(vector.begin(), vector.end(), 
+		ostream_iterator<V>(cout, " "));
+	cout << '\n';
+};
+
+int main(){
+	
+	ofstream outfile;
+	outfile.open("records.txt");
+	
+	
+	string data;
+	char character;
+	vector<char> characters;
+	
+	cout << "Enter characters: ";
+	getline(cin, data);
+	istringstream stream(data);
+	while (stream >> character)
+		characters.push_back(character);
+	
+	vector_out(characters);
+	
+	
+	outfile << data;
+	outfile.close();
+	
+	return 0;
+
+};
