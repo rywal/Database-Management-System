e8ce8b55aeb46193c7148ae50e3d1e91de9ad01e
Jonathan Grimes <jonygrimes@tamu.edu>
2016-02-03 23:40:59 -0600

    Merge Conflict Error

--- test.cpp
+++ test.cpp
@@ -7,12 +7,17 @@
 #include "Tuple.h"
 
 void main(){
-	//Create Relation
 	Database DB("DB");
+	printf ("Create new Relation A:\n");
 	string attribute_names1[] = {"Name","ID","ShoeSize", "DogName"};
 	int attribute_types1[] = {10, 0, 0, 10};
 	string primary_keys_names1[] = {"Name","ID"};
 	DB.create_relation("Relation A", attribute_names1, attribute_types1);
-	DB.relation("Relation A"]
+	
+	//DB.relation[get_relation_index("Relation A")]
+	DB.print_relation("Relation A");
+	printf("Select Relation A with all rows that ha")
+	printf ("Create new Relation B:\n");
+	
 
 }
