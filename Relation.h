94f0cf156d54368f4fb4d5d5da5814f88463b0d4
Ryan Walters <ryanwalters@tamu.edu>
2016-02-15 20:39:52 -0600

    working core files

--- Relation.h
+++ Relation.h
@@ -16,7 +16,6 @@ public:
     AttributeList attribute_list;
     std::vector<string> primary_keys;
 
-	Relation() {}
     Relation(
              string _name,
              vector<string> attribute_names,         // Name of each attribute
