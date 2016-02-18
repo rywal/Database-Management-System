7172c99450286e4c8da528727cc04368756b52b2
Victor Vazquez <victormanuel@tamu.edu>
2016-02-18 12:01:35 -0600

    added app file

---
+++ Application/app.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+
+//App
+
+using namespace std;
+
+int main(){
+
+	int select;
+	
+	cout << "\n Welcome to The Convention Exhibit Management System \n";
+	cout << " ----------------------------------------------- \n \n ";
+	cout << "1. Exhibit Manager \n 2. Exhibitor \n 3. Attendee \n 4. Exit \n \n";
+	cout << "Please select which data records you would like to access: ";
+	cin >> select;
+	
+	return 0
+	
+	;}
